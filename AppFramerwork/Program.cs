4554d96c266b059207473fe493fca04d079d03af
Cleber Marques <clebermarques1984@gmail.com>
2021-04-11 17:20:25 -0300

    initial commit

---
+++ AppFramerwork/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace AppFramerwork
+{
+	public static class Program
+	{
+		public static void Main(string[] args)
+		{
+			if (!args.Any())
+				throw new ArgumentException("Args deve ser preenchido", nameof(args));
+
+			Console.WriteLine("Aux app inicio");
+
+			foreach (var item in args)
+			{
+				Console.WriteLine(item);
+			}
+
+			Console.WriteLine("Aux app fim");
+		}
+	}
+}
---
+++ Playground/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace Playground
+{
+	public static class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("App inicio");
+
+			var path = Directory.GetCurrentDirectory();
+
+			var exePath = @$"{path}\aux-app\AppFramerwork.exe";
+
+			var items = new List<string> { "item1", "item2", "item3" };
+
+			ProcessStartInfo info = new ProcessStartInfo(exePath, string.Join(" ", items));
+
+			Process p = Process.Start(info);
+
+			p.WaitForExit();
+
+			Console.WriteLine($"Aux app exitcode = {p.ExitCode}");
+
+			Console.WriteLine("App fim");
+
+			Console.ReadLine();
+		}
+	}
+}
